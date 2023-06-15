73b7394be1c63fe12b3b4dc99413f34c04cf98e2
Lan Tran <lan.tran@asnet.com.vn>
2023-06-15 18:05:08 +0700

    add put, post, delete method to todo api

--- example/lib/data/network/http/todos.dart
+++ example/lib/data/network/http/todos.dart
@@ -5,10 +5,17 @@ import 'package:http/http.dart' as http;
 import '../../models/todo.dart';
 
 class HttpTodos {
-  static const String url = 'https://jsonplaceholder.typicode.com/todos';
+  final String baseUrl = 'https://648ae68b17f1536d65e9ecfd.mockapi.io/todos';
+  // final String baseUrl = 'https://648ae68b17f1536d65e9ecfd.mockapi.io/todos2';
+
+  final Map<String, String> headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+
   Future<List<Todo>> fetchTodos() async {
+    final Uri url = Uri.parse(baseUrl);
     try {
-      final response = await http.get(Uri.parse(url));
+      final response = await http.get(url);
       List<Todo> todosList = [];
 
       if (response.statusCode == 200) {
@@ -17,6 +24,41 @@ class HttpTodos {
             .toList();
       }
       return todosList;
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> addTodo(Todo todo) async {
+    final Uri url = Uri.parse(baseUrl);
+    try {
+      await http.post(
+        url,
+        headers: headers,
+        body: json.encode(todo.toJson()),
+      );
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> updateTodo(Todo todo) async {
+    final Uri url = Uri.parse('$baseUrl/${todo.id}');
+    try {
+      await http.put(
+        url,
+        headers: headers,
+        body: json.encode(todo.toJson()),
+      );
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  Future<void> deleteTodo(String todoId) async {
+    final Uri url = Uri.parse('$baseUrl/$todoId');
+    try {
+      await http.delete(url);
     } catch (e) {
       throw Exception(e.toString());
     }
