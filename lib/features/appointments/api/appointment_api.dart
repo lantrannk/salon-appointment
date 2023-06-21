89034c975783cb7f215f5d83e1eac5e4d9e793bb
Lan Tran <lan.tran@asnet.com.vn>
2023-06-21 19:09:45 +0700

    define constant: api url

--- lib/features/appointments/api/appointment_api.dart
+++ lib/features/appointments/api/appointment_api.dart
@@ -5,8 +5,10 @@ import 'package:salon_appointment/features/appointments/model/appointment.dart';
 import '../../../core/constants/constants.dart';
 
 class AppointmentApi {
+  static const String baseUrl = '$apiUrl/appointments';
+
   static Future<String> getAppointments() async {
-    final url = Uri.parse('$apiUrl/appointments');
+    final url = Uri.parse(baseUrl);
 
     final response = await http.get(url);
 
@@ -14,7 +16,7 @@ class AppointmentApi {
   }
 
   static Future<void> addAppointment(Appointment appointment) async {
-    final url = Uri.parse('$apiUrl/appointments');
+    final url = Uri.parse(baseUrl);
     final headers = {'Content-Type': 'application/json'};
     final map = appointment.toJson();
     final body = json.encode(map);
@@ -23,7 +25,7 @@ class AppointmentApi {
   }
 
   static Future<void> updateAppointment(Appointment appointment) async {
-    final url = Uri.parse('$apiUrl/appointments/${appointment.id}');
+    final url = Uri.parse('$baseUrl/${appointment.id}');
     final headers = {'Content-Type': 'application/json'};
     final map = appointment.toJson();
     final body = json.encode(map);
@@ -32,7 +34,7 @@ class AppointmentApi {
   }
 
   static Future<void> deleteAppointment(String appointmentId) async {
-    final url = Uri.parse('$apiUrl/appointments/$appointmentId');
+    final url = Uri.parse('$baseUrl/$appointmentId');
 
     await http.delete(url);
   }
