705600aca05bf5659457ace999f5a0641e9e89c4
Lan Tran <lan.tran@asnet.com.vn>
2023-06-21 19:13:29 +0700

    update repository: get appointments from storage

--- lib/features/appointments/repository/appointment_repository.dart
+++ lib/features/appointments/repository/appointment_repository.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
+import '../../../core/storage/appointment_storage.dart';
 import '../../../core/storage/user_storage.dart';
-import '../api/appointment_api.dart';
 import '../model/appointment.dart';
 
 class AppointmentRepository {
@@ -9,8 +9,8 @@ class AppointmentRepository {
     final Map<String, dynamic> userJson = await UserStorage.getUser();
 
     final appointments = (userJson['isAdmin'])
-        ? await AppointmentApi.getAppointments()
-        : await AppointmentApi.getAppointmentsOfUser(userJson['id']);
+        ? await AppointmentStorage.getAppointments()
+        : await AppointmentStorage.getAppointmentsOfUser(userJson['id']);
     final dateStr = DateFormat.yMd().format(date);
 
     final List<Appointment> appointmentsOfDate = appointments
