a3990ef5b5331bf1fd198664024da1d1e331fbf6
Lan Tran <lan.tran@asnet.com.vn>
2023-06-21 19:20:49 +0700

    update user api

--- lib/features/auth/api/user_api.dart
+++ lib/features/auth/api/user_api.dart
@@ -1,18 +1,13 @@
-import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:salon_appointment/features/auth/model/user.dart';
 
 import '../../../core/constants/constants.dart';
 
 class UserApi {
-  static Future<List<User>> getUsers() async {
+  static Future<String> getUsers() async {
     final url = Uri.parse('$apiUrl/users');
 
     final response = await http.get(url);
-    final responseData = (json.decode(response.body) as List)
-        .map((user) => User.fromJson(user as Map<String, dynamic>))
-        .toList();
 
-    return responseData;
+    return response.body;
   }
 }
