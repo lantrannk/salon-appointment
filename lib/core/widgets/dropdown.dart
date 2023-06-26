3bf2c2f7f48e6d0d43e9aa45d2f0e723beee89fc
Lan Tran <lan.tran@asnet.com.vn>
2023-06-26 09:30:40 +0700

    remove fixed width & height

--- lib/core/widgets/dropdown.dart
+++ lib/core/widgets/dropdown.dart
@@ -7,39 +7,47 @@ class Dropdown extends StatelessWidget {
     required this.items,
     required this.selectedValue,
     required this.onChanged,
+    this.height = 44,
     super.key,
   });
 
   final List<String> items;
   final String? selectedValue;
+  final double? height;
   final Function(String?)? onChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      height: 44,
-      width: 345,
-      decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.onPrimary,
-          borderRadius: BorderRadius.circular(10),
-          border: Border.all(
-            color: Theme.of(context).colorScheme.secondaryContainer,
-          )),
-      child: DropdownButton<String>(
-        hint: const Text('Select Services'),
-        value: selectedValue,
-        onChanged: onChanged,
-        items: items
-            .map<DropdownMenuItem<String>>((value) => DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                ))
-            .toList(),
-        icon: const Icon(Assets.dropdownIcon),
-        iconSize: 24,
-        underline: const SizedBox(),
-        isExpanded: true,
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        maxHeight: 44,
+      ),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        height: height,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.onPrimary,
+            borderRadius: BorderRadius.circular(10),
+            border: Border.all(
+              color: Theme.of(context).colorScheme.secondaryContainer,
+            )),
+        child: DropdownButton<String>(
+          hint: const Text('Select Services'),
+          value: selectedValue,
+          onChanged: onChanged,
+          items: items
+              .map<DropdownMenuItem<String>>(
+                  (value) => DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      ))
+              .toList(),
+          icon: const Icon(Assets.dropdownIcon),
+          iconSize: 24,
+          underline: const SizedBox(),
+          isExpanded: true,
+        ),
       ),
     );
   }
