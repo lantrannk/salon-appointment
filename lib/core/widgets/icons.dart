96c66dc7ba32aac8af005e41ad616224271b6b4a
Lan Tran <lan.tran@asnet.com.vn>
2023-06-27 18:07:12 +0700

    create gradient icon widget

--- lib/core/widgets/icons.dart
+++ lib/core/widgets/icons.dart
@@ -23,3 +23,38 @@ class SAIcons extends StatelessWidget {
     );
   }
 }
+
+class GradientIcon extends StatelessWidget {
+  const GradientIcon({
+    required this.icon,
+    required this.gradient,
+    this.size = 20,
+    this.onTap,
+    super.key,
+  });
+  final IconData icon;
+  final double? size;
+  final Gradient gradient;
+  final VoidCallback? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: GestureDetector(
+        onTap: onTap,
+        child: ShaderMask(
+          child: Icon(
+            icon,
+            size: size,
+          ),
+          shaderCallback: (bounds) {
+            final Rect rect = Rect.fromLTRB(0, 0, size!, size!);
+            return gradient.createShader(rect);
+          },
+        ),
+      ),
+    );
+  }
+}
