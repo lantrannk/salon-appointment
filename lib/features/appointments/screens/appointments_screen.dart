534a4bab23e7f31ffa43ebe268bcf434b1d40739
Lan Tran <lan.tran@asnet.com.vn>
2023-06-29 14:07:17 +0700

    create cell widget

--- lib/features/appointments/screens/appointments_screen.dart
+++ lib/features/appointments/screens/appointments_screen.dart
@@ -547,3 +547,50 @@ class Description extends StatelessWidget {
     );
   }
 }
+
+class CalendarCell extends StatelessWidget {
+  const CalendarCell({
+    required this.dayOfWeek,
+    required this.day,
+    this.dayOfWeekColor,
+    this.dayColor,
+    this.bgColor,
+    this.gradient,
+    super.key,
+  });
+
+  final Color? dayOfWeekColor;
+  final Color? dayColor;
+  final Color? bgColor;
+  final Gradient? gradient;
+  final String dayOfWeek;
+  final String day;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: bgColor,
+        gradient: gradient,
+        shape: BoxShape.rectangle,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SAText.weekCalendarCell(
+            text: dayOfWeek,
+            color: dayOfWeekColor ?? colorScheme.onPrimary.withOpacity(0.6429),
+          ),
+          SAText.weekCalendarCell(
+            text: day,
+            color: dayColor ?? colorScheme.onPrimary,
+          ),
+        ],
+      ),
+    );
+  }
+}
