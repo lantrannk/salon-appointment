4389d5ed5f2a3d5b1ecf46d65b91d312d2a76438
Lan Tran <lan.tran@asnet.com.vn>
2023-07-05 18:14:29 +0700

    create time row widget in appointment card

---
+++ lib/features/appointments/screens/appointments_widgets/time.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/constants/assets.dart';
+import '../../../../core/utils.dart';
+import '../../../../core/widgets/widgets.dart';
+
+class Time extends StatelessWidget {
+  const Time({
+    required this.startTime,
+    required this.endTime,
+    required this.text,
+    super.key,
+  });
+
+  final DateTime startTime;
+  final DateTime endTime;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = Theme.of(context);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            SAIcons(
+              icon: Assets.scheduleIcon,
+              size: 20,
+              color: themeData.colorScheme.tertiary,
+            ),
+            const SizedBox(
+              width: 12,
+            ),
+            Text(
+              '${formatTime(startTime)} - ${formatTime(endTime)}',
+              style: themeData.textTheme.bodyLarge!.copyWith(
+                height: 24 / 14,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const SizedBox(
+              width: 32,
+            ),
+            Text(
+              text,
+              style: themeData.textTheme.bodySmall!.copyWith(
+                color: themeData.colorScheme.onSecondary,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
