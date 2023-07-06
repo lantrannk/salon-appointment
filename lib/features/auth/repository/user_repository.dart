2039b6e3fec7788a252a07a7529b190ef0b35a1b
Lan Tran <lan.tran@asnet.com.vn>
2023-07-06 17:35:34 +0700

    remove user from storage

--- lib/features/auth/repository/user_repository.dart
+++ lib/features/auth/repository/user_repository.dart
@@ -1,10 +1,17 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
 import '../../../core/storage/user_storage.dart';
 import '../model/user.dart';
 
 class UserRepository {
-  Future<List<User>> load() async {
+  static Future<List<User>> load() async {
     await UserStorage.setUsers();
     final users = await UserStorage.getUsers();
     return users;
+  }
+
+  static Future<void> removeUser() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.remove('user');
   }
 }
