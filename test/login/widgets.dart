7a0544dc660c47793a7e9f03ce129a2ffdc82b31
Lan Tran <lan.tran@asnet.com.vn>
2023-07-19 11:16:16 +0700

    test widgets in login screen

--- test/login/widgets.dart
+++ test/login/widgets.dart
@@ -1,4 +1,3 @@
-import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -7,39 +6,51 @@ import 'package:salon_appointment/core/generated/l10n.dart';
 import 'package:salon_appointment/features/auth/bloc/auth_bloc.dart';
 import 'package:salon_appointment/features/auth/screens/login_screen.dart';
 
-class MockAuthBloc extends MockBloc<AuthEvent, AuthState> implements AuthBloc {}
-
 void main() {
-  group('test text input -', () {
-    late AuthBloc authBloc;
+  late AuthBloc authBloc;
+  late Widget loginScreen;
 
-    setUp(() {
-      authBloc = MockAuthBloc();
-    });
+  setUpAll(() {
+    authBloc = AuthBloc();
+    loginScreen = MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
+      home: BlocProvider.value(
+        value: authBloc,
+        child: const LoginScreen(),
+      ),
+    );
+  });
 
-    tearDown(() {
-      authBloc.close();
+  tearDownAll(() {
+    authBloc.close();
+  });
+
+  group('test login screen widgets -', () {
+    testWidgets('Login Screen has one logo text', (tester) async {
+      await tester.pumpWidget(loginScreen);
+      await tester.pumpAndSettle();
+
+      expect(find.text('avisit'), findsOneWidget);
     });
 
     testWidgets('Login Screen has two text form field', (tester) async {
-      await tester.pumpWidget(
-        MaterialApp(
-          localizationsDelegates: const [
-            S.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: S.delegate.supportedLocales,
-          home: BlocProvider.value(
-            value: authBloc,
-            child: const LoginScreen(),
-          ),
-        ),
-      );
+      await tester.pumpWidget(loginScreen);
       await tester.pumpAndSettle();
 
       expect(find.byType(TextFormField), findsNWidgets(2));
+    });
+
+    testWidgets('Login Screen has one outlined button', (tester) async {
+      await tester.pumpWidget(loginScreen);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(OutlinedButton), findsOneWidget);
     });
   });
 }
