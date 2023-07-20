0bee3a29ed8f1ca61d14f1ab8ebdbe09f45206b8
Lan Tran <lan.tran@asnet.com.vn>
2023-07-20 17:56:15 +0700

    rename test file

---
+++ test/utils/break_time_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:salon_appointment/core/utils.dart';
+
+void main() {
+  group('test conflict break time -', () {
+    test('time is before 12:00 PM then return false', () {
+      final time = DateTime(2023, 7, 19, 11, 59);
+
+      expect(isBreakTime(time), false);
+    });
+
+    test('time is 12:00 PM then return false', () {
+      final time = DateTime(2023, 7, 19, 12, 00);
+
+      expect(isBreakTime(time), true);
+    });
+
+    test('time is after 12:00 PM then return true', () {
+      final time = DateTime(2023, 7, 19, 12, 1);
+
+      expect(isBreakTime(time), true);
+    });
+
+    test('time is before 3:20 PM then return true', () {
+      final time = DateTime(2023, 7, 19, 15, 7);
+
+      expect(isBreakTime(time), true);
+    });
+
+    test('time is 3:20 PM then return true', () {
+      final time = DateTime(2023, 7, 19, 15, 20);
+
+      expect(isBreakTime(time), false);
+    });
+
+    test('time is after 3:20 PM then return false', () {
+      final time = DateTime(2023, 7, 19, 15, 21);
+
+      expect(isBreakTime(time), false);
+    });
+  });
+}
