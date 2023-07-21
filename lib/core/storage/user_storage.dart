71754c385432e93d584c89c6d38e3f2fceaa024b
Lan Tran <lan.tran@asnet.com.vn>
2023-07-21 17:03:15 +0700

    define shared preferences in user storage

--- lib/core/storage/user_storage.dart
+++ lib/core/storage/user_storage.dart
@@ -7,19 +7,23 @@ import '../../features/auth/api/user_api.dart';
 import '../../features/auth/model/user.dart';
 
 class UserStorage {
+  static final Future<SharedPreferences> _prefs =
+      SharedPreferences.getInstance();
+
   /// Save a [List] of [String] user encode
   static Future<void> setUsers() async {
+    final SharedPreferences prefs = await _prefs;
+
     final UserApi userApi = UserApi();
     final String users = await userApi.getUsers(http.Client());
 
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setString('users', users);
   }
 
   /// Returns a [List] of [User] from storage
   static Future<List<User>> getUsers() async {
     await setUsers();
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     final String usersStr = prefs.getString('users')!;
     final List<User> users =
         (json.decode(usersStr) as List).map((e) => User.fromJson(e)).toList();
@@ -29,14 +33,14 @@ class UserStorage {
 
   /// Returns a [Map] of a user from storage
   static Future<void> setUser(User user) async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     final String userEncode = jsonEncode(user.toJson());
     await prefs.setString('user', userEncode);
   }
 
   /// Returns a [Map] of a user from storage
   static Future<User?> getUser() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
     final String? userStr = prefs.getString('user');
     return userStr != null ? User.fromJson(jsonDecode(userStr)) : null;
   }
