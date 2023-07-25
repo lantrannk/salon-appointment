8045297524ea84dd84e29fe9dd1bc5e153755a60
Lan Tran <lan.tran@asnet.com.vn>
2023-07-25 18:38:57 +0700

    fix: test outlined button

--- test/widgets/outlined_button_test.dart
+++ test/widgets/outlined_button_test.dart
@@ -1,67 +1,62 @@
+import 'dart:io' as io;
+
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http;
 import 'package:mocktail/mocktail.dart';
 import 'package:salon_appointment/core/generated/l10n.dart';
+import 'package:salon_appointment/features/appointments/bloc/appointment_bloc.dart';
 import 'package:salon_appointment/features/auth/bloc/auth_bloc.dart';
 import 'package:salon_appointment/features/auth/screens/login_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MockAuthBloc extends Mock implements AuthBloc {}
 
-class MockClient extends Mock implements http.Client {}
+class MockAppointmentBloc extends Mock implements AppointmentBloc {}
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  io.HttpOverrides.global = null;
+
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+  });
+
   group('test outlined button -', () {
-    late AuthBloc authBloc;
-    late Widget loginScreen;
-    late List<AuthState> expectedStates;
-    late http.Client client;
-
-    const expectedStr =
-        '[{"phoneNumber":"0794542105","name":"Lan Tran","avatar":"https://bazaarvietnam.vn/wp-content/uploads/2020/02/selena-gomez-rare-beauty-launch-03-09-2020-00-thumb.jpg","password":"123456","isAdmin":true,"id":"1"},{"phoneNumber":"0905999222","name":"Carol Williams","avatar":"https://assets.vogue.in/photos/611cf20c8733032148fe1b06/2:3/w_2560%2Cc_limit/Slide%25201.jpg","password":"123456","isAdmin":false,"id":"2"},{"phoneNumber":"0934125689","name":"Hailee Steinfeld","avatar":"https://glints.com/vn/blog/wp-content/uploads/2022/08/co%CC%82ng-vie%CC%A3%CC%82c-beauty-blogger-819x1024.jpg","password":"123456","isAdmin":false,"id":"3"},{"phoneNumber":"0902335577","name":"Ruby Nguyen","avatar":"https://studiochupanhdep.com//Upload/Images/Album/anh-beauty-11.jpg","password":"123456","isAdmin":false,"id":"4"},{"phoneNumber":"070456123","name":"Elizabeth Taylor","avatar":"https://www.m1-beauty.de/assets/images/f/Ronja_VorherNachher_45Grad_2-c7cc48ca.jpg","password":"123456","isAdmin":false,"id":"5"}]';
-    setUp(() async {
-      authBloc = MockAuthBloc();
-      client = MockClient();
-      loginScreen = MaterialApp(
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        home: BlocProvider.value(
-          value: authBloc,
-          child: const LoginScreen(),
-        ),
-      );
-
-      when(
-        () => client.get(
-          Uri.parse('https://63ab8e97fdc006ba60609b9b.mockapi.io/users'),
-        ),
-      ).thenAnswer(
-        (_) async => http.Response(
-          expectedStr,
-          200,
-        ),
-      );
-
-      expectedStates = [
-        LoginLoading(),
-        const LoginError('invalid-account'),
-      ];
-      whenListen(
-        authBloc,
-        Stream.fromIterable(expectedStates),
-        initialState: LoginLoading(),
-      );
-    });
-
     group('Press login button with invalid phone number -', () {
+      late AuthBloc authBloc;
+      late Widget loginScreen;
+      late List<AuthState> expectedStates;
+
+      setUp(() async {
+        authBloc = MockAuthBloc();
+        loginScreen = MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: BlocProvider.value(
+            value: authBloc,
+            child: const LoginScreen(),
+          ),
+        );
+
+        expectedStates = [
+          LoginLoading(),
+          const LoginError('invalid-account'),
+        ];
+        whenListen(
+          authBloc,
+          Stream.fromIterable(expectedStates),
+          initialState: LoginLoading(),
+        );
+      });
+
       testWidgets('phone number is blank', (tester) async {
         await tester.pumpWidget(loginScreen);
         await tester.enterText(find.byType(TextFormField).first, '');
@@ -125,6 +120,37 @@ void main() {
     });
 
     group('Press login button with invalid password -', () {
+      late AuthBloc authBloc;
+      late Widget loginScreen;
+      late List<AuthState> expectedStates;
+
+      setUp(() async {
+        authBloc = MockAuthBloc();
+        loginScreen = MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: BlocProvider.value(
+            value: authBloc,
+            child: const LoginScreen(),
+          ),
+        );
+
+        expectedStates = [
+          LoginLoading(),
+          const LoginError('invalid-account'),
+        ];
+        whenListen(
+          authBloc,
+          Stream.fromIterable(expectedStates),
+          initialState: LoginLoading(),
+        );
+      });
+
       testWidgets('password is blank', (tester) async {
         await tester.pumpWidget(loginScreen);
         await tester.enterText(find.byType(TextFormField).last, '');
@@ -157,10 +183,57 @@ void main() {
     });
 
     group('Press login button with incorrect account -', () {
+      late AuthBloc authBloc;
+      late Widget loginScreen;
+      late List<AuthState> expectedStates;
+
+      setUp(() async {
+        authBloc = MockAuthBloc();
+        loginScreen = MaterialApp(
+          localizationsDelegates: const [
+            S.delegate,
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: S.delegate.supportedLocales,
+          home: BlocProvider.value(
+            value: authBloc,
+            child: const LoginScreen(),
+          ),
+        );
+
+        expectedStates = [
+          LoginLoading(),
+          const LoginError('incorrect-account'),
+        ];
+        whenListen(
+          authBloc,
+          Stream.fromIterable(expectedStates),
+          initialState: LoginLoading(),
+        );
+      });
+
       testWidgets('phone number is not exist', (tester) async {
         await tester.pumpWidget(loginScreen);
         await tester.enterText(find.byType(TextFormField).first, '0905123456');
         await tester.enterText(find.byType(TextFormField).last, '123456');
+        await tester.tap(find.widgetWithText(OutlinedButton, 'Log in'));
+        await tester.pump();
+
+        expect(
+          find.widgetWithText(
+            SnackBar,
+            'Phone number or Password is incorrect.',
+          ),
+          findsOneWidget,
+        );
+      });
+
+      testWidgets('password is not correct', (tester) async {
+        await tester.pumpWidget(loginScreen);
+        await tester.enterText(find.byType(TextFormField).first, '0905999222');
+        await tester.enterText(find.byType(TextFormField).last, '123456abcd');
         await tester.tap(find.widgetWithText(OutlinedButton, 'Log in'));
         await tester.pump();
 
