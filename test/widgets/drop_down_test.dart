9462a3d2e9af89187434ed70bfe961a668a10a68
Lan Tran <lan.tran@asnet.com.vn>
2023-07-31 20:38:15 +0700

    test drop down widget

---
+++ test/widgets/drop_down_test.dart
@@ -0,0 +1,137 @@
+import 'dart:io' as io;
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:salon_appointment/core/generated/l10n.dart';
+import 'package:salon_appointment/features/appointments/bloc/appointment_bloc.dart'
+    as appointment;
+import 'package:salon_appointment/features/appointments/screens/new_appointment_screen.dart';
+import 'package:salon_appointment/features/auth/model/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MockAppointmentBloc extends Mock implements appointment.AppointmentBloc {}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  io.HttpOverrides.global = null;
+
+  late appointment.AppointmentBloc appointmentBloc;
+  late Widget newAppointmentScreen;
+  late List<appointment.AppointmentState> expectedStates;
+  late Finder initialDropDownFinder;
+  late Finder backDropDownFinder;
+  late Finder servicesFinder;
+
+  setUpAll(() async {
+    SharedPreferences.setMockInitialValues({});
+
+    appointmentBloc = MockAppointmentBloc();
+    newAppointmentScreen = MediaQuery(
+      data: const MediaQueryData(),
+      child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        home: BlocProvider.value(
+          value: appointmentBloc,
+          child: NewAppointmentScreen(
+            selectedDay: DateTime.now(),
+          ),
+        ),
+      ),
+    );
+
+    final user = User.fromJson({
+      'phoneNumber': '0794542105',
+      'name': 'Lan Tran',
+      'avatar':
+          'https://bazaarvietnam.vn/wp-content/uploads/2020/02/selena-gomez-rare-beauty-launch-03-09-2020-00-thumb.jpg',
+      'password': '123456',
+      'isAdmin': true,
+      'id': '1'
+    });
+
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setString('user',
+        '{"phoneNumber":"0794542105","name":"Lan Tran","avatar":"https://bazaarvietnam.vn/wp-content/uploads/2020/02/selena-gomez-rare-beauty-launch-03-09-2020-00-thumb.jpg","password":"123456","isAdmin":true,"id":"1"}');
+
+    expectedStates = [
+      appointment.UserLoaded(user),
+    ];
+    whenListen(
+      appointmentBloc,
+      Stream.fromIterable(expectedStates),
+      initialState: appointment.UserLoaded(user),
+    );
+
+    initialDropDownFinder = find.widgetWithText(
+      DropdownButton<String>,
+      'Select Services',
+    );
+    backDropDownFinder = find.widgetWithText(
+      DropdownButton<String>,
+      'Back',
+    );
+    servicesFinder = find.byType(DropdownMenuItem<String>);
+  });
+
+  testWidgets(
+    'New Appointment Screen has one drop down button',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(newAppointmentScreen);
+        await tester.pump(const Duration(seconds: 1));
+
+        // Drop down button
+        expect(initialDropDownFinder, findsOneWidget);
+      });
+    },
+  );
+
+  testWidgets(
+    'Show list of 3 items when pressing drop down button',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(newAppointmentScreen);
+        await tester.pumpAndSettle();
+
+        // Press drop down button
+        await tester.tap(initialDropDownFinder);
+        await tester.pumpAndSettle();
+
+        // Drop down has 3 items
+        expect(servicesFinder, findsNWidgets(3));
+      });
+    },
+  );
+
+  testWidgets(
+    'Show drop down with selected item when pressing item on list',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(newAppointmentScreen);
+        await tester.pumpAndSettle();
+
+        // Press drop down button
+        await tester.tap(initialDropDownFinder);
+        await tester.pumpAndSettle();
+
+        // Press first item of list
+        await tester.tap(servicesFinder.first, warnIfMissed: false);
+        await tester.pumpAndSettle();
+
+        // Show drop down with the first item: 'Back'
+        expect(backDropDownFinder, findsOneWidget);
+      });
+    },
+  );
+}
