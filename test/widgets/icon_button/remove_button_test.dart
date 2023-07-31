a09ebed024b9f4015911fd7bb3b8bbf0de6be295
Lan Tran <lan.tran@asnet.com.vn>
2023-07-31 20:23:30 +0700

    test remove icon button on appointment screen

---
+++ test/widgets/icon_button/remove_button_test.dart
@@ -0,0 +1,153 @@
+import 'dart:io' as io;
+
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:salon_appointment/core/constants/assets.dart';
+import 'package:salon_appointment/core/generated/l10n.dart';
+import 'package:salon_appointment/features/appointments/bloc/appointment_bloc.dart';
+import 'package:salon_appointment/features/appointments/model/appointment.dart';
+import 'package:salon_appointment/features/appointments/screens/appointments_screen.dart';
+
+import 'package:salon_appointment/features/auth/model/user.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class MockAppointmentBloc extends Mock implements AppointmentBloc {}
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  io.HttpOverrides.global = null;
+
+  late AppointmentBloc appointmentBloc;
+  late Widget appointmentScreen;
+  late List<AppointmentState> expectedStates;
+  late Finder removeButtonFinder;
+
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+
+    appointmentBloc = MockAppointmentBloc();
+    appointmentScreen = MediaQuery(
+      data: const MediaQueryData(),
+      child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        home: BlocProvider.value(
+          value: appointmentBloc,
+          child: AppointmentScreen(
+            focusedDay: DateTime(2023, 08, 15),
+          ),
+        ),
+      ),
+    );
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('user',
+        '{"phoneNumber":"0794542105","name":"Lan Tran","avatar":"https://bazaarvietnam.vn/wp-content/uploads/2020/02/selena-gomez-rare-beauty-launch-03-09-2020-00-thumb.jpg","password":"123456","isAdmin":true,"id":"1"}');
+
+    final users = [
+      {
+        'phoneNumber': '0794542105',
+        'name': 'Lan Tran',
+        'avatar':
+            'https://bazaarvietnam.vn/wp-content/uploads/2020/02/selena-gomez-rare-beauty-launch-03-09-2020-00-thumb.jpg',
+        'password': '123456',
+        'isAdmin': true,
+        'id': '1'
+      },
+      {
+        'phoneNumber': '0905999222',
+        'name': 'Carol Williams',
+        'avatar':
+            'https://assets.vogue.in/photos/611cf20c8733032148fe1b06/2:3/w_2560%2Cc_limit/Slide%25201.jpg',
+        'password': '123456',
+        'isAdmin': false,
+        'id': '2'
+      },
+    ].map((e) => User.fromJson(e)).toList();
+
+    final appointments = [
+      {
+        'date': '2023-08-15T10:55:00.000',
+        'startTime': '2023-08-15T18:00:00.000',
+        'endTime': '2023-08-15T18:30:00.000',
+        'userId': '2',
+        'services': 'Non-Invasive Body Contouring',
+        'description': 'Nothing to write.',
+        'isCompleted': false,
+        'id': '79'
+      },
+    ].map((e) => Appointment.fromJson(e)).toList();
+
+    expectedStates = [
+      AppointmentLoading(),
+      AppointmentLoadSuccess(
+        users: users,
+        appointments: appointments,
+      ),
+    ];
+    whenListen(
+      appointmentBloc,
+      Stream.fromIterable(expectedStates),
+      initialState: AppointmentLoading(),
+    );
+
+    removeButtonFinder = find.widgetWithIcon(
+      IconButton,
+      Assets.removeIcon,
+    );
+  });
+
+  testWidgets(
+    'Appointment Screen has one remove icon button on each appointment card',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(appointmentScreen);
+        await tester.pump();
+
+        // Loading appointments
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.pumpAndSettle();
+
+        // Remove icon button
+        expect(removeButtonFinder, findsOneWidget);
+      });
+    },
+  );
+
+  testWidgets(
+    'Show confirm dialog when pressing remove icon button',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpWidget(appointmentScreen);
+        await tester.pump();
+
+        // Loading appointments
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.pumpAndSettle();
+
+        // Press remove icon button
+        await tester.tap(removeButtonFinder);
+        await tester.pumpAndSettle();
+
+        // Show confirm dialog when pressing remove icon button
+        expect(
+          find.widgetWithText(Dialog, 'Remove Appointment'),
+          findsOneWidget,
+        );
+      });
+    },
+  );
+}
