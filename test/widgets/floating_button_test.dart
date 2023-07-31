9a8579d60650a777c235d1ccd8b2bd2d2bea9ad3
Lan Tran <lan.tran@asnet.com.vn>
2023-07-31 11:39:21 +0700

    update test floating action button

--- test/widgets/floating_button_test.dart
+++ test/widgets/floating_button_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:salon_appointment/core/constants/assets.dart';
 import 'package:salon_appointment/core/generated/l10n.dart';
 import 'package:salon_appointment/features/appointments/bloc/appointment_bloc.dart';
 import 'package:salon_appointment/features/appointments/screens/calendar_screen.dart';
@@ -85,7 +86,17 @@ void main() {
       await tester.pumpWidget(calendarScreen);
       await tester.pump(const Duration(seconds: 1));
 
-      expect(find.byType(FloatingActionButton), findsOneWidget);
+      // Add floating action button
+      expect(
+        find.widgetWithIcon(FloatingActionButton, Assets.addIcon),
+        findsOneWidget,
+      );
+
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      // Navigate to new appointment screen when pressing add FAB
+      expect(find.text('New Appointment'), findsOneWidget);
     });
   });
 }
