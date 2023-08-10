aef0ea120f962faf18f20447dea9ac64f0c3d52a
Lan Tran <lan.tran@asnet.com.vn>
2023-08-10 18:38:17 +0700

    test common widget: input

--- test/widgets/input_test.dart
+++ test/widgets/input_test.dart
@@ -1,244 +1,87 @@
-import 'package:bloc_test/bloc_test.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
-import 'package:salon_appointment/core/generated/l10n.dart';
-import 'package:salon_appointment/features/auth/bloc/auth_bloc.dart';
-import 'package:salon_appointment/features/auth/screens/login_screen.dart';
+import 'dart:io' as io;
 
-class MockAuthBloc extends Mock implements AuthBloc {}
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:salon_appointment/core/widgets/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../pump_widgets/common_widget.dart';
 
 void main() {
-  late AuthBloc authBloc;
-  late Widget loginScreen;
-  late List<AuthState> expectedStates;
+  TestWidgetsFlutterBinding.ensureInitialized();
+  io.HttpOverrides.global = null;
 
-  group('test input widget -', () {
-    setUp(() {
-      authBloc = MockAuthBloc();
-      loginScreen = MaterialApp(
-        localizationsDelegates: const [
-          S.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: S.delegate.supportedLocales,
-        home: BlocProvider.value(
-          value: authBloc,
-          child: const LoginScreen(),
-        ),
-      );
+  late Widget inputWidget;
 
-      expectedStates = [
-        LoginLoading(),
-        const LoginError('invalid-account'),
-      ];
-      whenListen(
-        authBloc,
-        Stream.fromIterable(expectedStates),
-        initialState: LoginLoading(),
-      );
+  late Finder hintTextInputFinder;
+  late Finder enteredTextInputFinder;
+
+  late TextEditingController textEditingController;
+  late VoidCallback onEditCompleted;
+  late FocusNode focusNode;
+  late List<int> log;
+
+  setUpAll(() async {
+    SharedPreferences.setMockInitialValues({});
+
+    textEditingController = TextEditingController();
+    focusNode = FocusNode();
+    log = [];
+    onEditCompleted = () => log.add(0);
+
+    hintTextInputFinder = find.widgetWithText(
+      TextFormField,
+      'Name',
+    );
+
+    enteredTextInputFinder = find.widgetWithText(
+      TextFormField,
+      'Lan Tran',
+    );
+
+    inputWidget = TestWidget(
+      body: Input(
+        text: 'Name',
+        controller: textEditingController,
+        focusNode: focusNode,
+        onEditCompleted: onEditCompleted,
+      ),
+    );
+  });
+
+  group('test input widget', () {
+    testWidgets('has a hint text', (tester) async {
+      await tester.pumpWidget(inputWidget);
+      await tester.pump();
+
+      // find a text form field with hint text
+      expect(hintTextInputFinder, findsOneWidget);
     });
 
-    testWidgets('Login Screen has two text form field', (tester) async {
-      // Run app with LoginScreen()
-      await tester.pumpWidget(loginScreen);
+    testWidgets('shows the text that was entered', (tester) async {
+      await tester.pumpWidget(inputWidget);
+      await tester.pump();
+
+      await tester.enterText(hintTextInputFinder, 'Lan Tran');
       await tester.pumpAndSettle();
 
-      expect(find.byType(TextFormField), findsNWidgets(2));
-      expect(
-        find.widgetWithText(TextFormField, 'Phone number'),
-        findsOneWidget,
-      );
-      expect(
-        find.widgetWithText(TextFormField, 'Password'),
-        findsOneWidget,
-      );
+      // find a text form field with entered text
+      expect(enteredTextInputFinder, findsOneWidget);
     });
 
-    group('phone number input -', () {
-      testWidgets(
-          'Enter an empty text to phone number input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
+    testWidgets(
+      'pressed ENTER then call onEditCompleted function 1 time',
+      (tester) async {
+        await tester.pumpWidget(inputWidget);
+        await tester.pump();
 
-        // Enter a less-than-10-digit to phone number input
-        await tester.enterText(find.byType(TextFormField).first, '123');
-
-        // Enter an empty text to phone number input
-        await tester.enterText(find.byType(TextFormField).first, '');
-
+        await tester.enterText(hintTextInputFinder, 'Lan Tran');
+        await tester.testTextInput.receiveAction(TextInputAction.done);
         await tester.pumpAndSettle();
 
-        // Error text: Phone number is blank.
-        expect(find.text('Phone number is blank.'), findsOneWidget);
-      });
-
-      testWidgets(
-          'Enter a less-than-10-digit text to phone number input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a less-than-10-digit to phone number input
-        await tester.enterText(find.byType(TextFormField).first, '123');
-
-        await tester.pumpAndSettle();
-
-        // Error text: Phone number must have 10 digits.
-        expect(find.text('Phone number must have 10 digits.'), findsOneWidget);
-      });
-
-      testWidgets(
-          'Enter a more-10-digit text to phone number input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a more-than-10-digit text to phone number input
-        await tester.enterText(
-            find.byType(TextFormField).first, '0905123456789');
-
-        await tester.pumpAndSettle();
-
-        // Error text: Phone number must have 10 digits.
-        expect(find.text('Phone number must have 10 digits.'), findsOneWidget);
-      });
-
-      testWidgets(
-          'Enter a not-number text to phone number input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a not-number text to phone number input
-        await tester.enterText(find.byType(TextFormField).first, 'test_phone');
-
-        await tester.pumpAndSettle();
-
-        // Error text: Phone number must be a string of digits.
-        expect(find.text('Phone number must be a string of digits.'),
-            findsOneWidget);
-      });
-
-      testWidgets('Enter valid text to phone number input after an invalid one',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a less-than-10-digit to phone number input
-        await tester.enterText(find.byType(TextFormField).first, '123');
-        await tester.pumpAndSettle();
-
-        // Error text: Phone number must have 10 digits.
-        expect(find.text('Phone number must have 10 digits.'), findsOneWidget);
-
-        // Enter an empty text to phone number input
-        await tester.enterText(find.byType(TextFormField).first, '');
-        await tester.pumpAndSettle();
-
-        // Error text: Replaced by Phone number is blank.
-        expect(find.text('Phone number must have 10 digits.'), findsNothing);
-        expect(find.text('Phone number is blank.'), findsOneWidget);
-
-        // Enter a more-than-10-digit text to phone number input
-        await tester.enterText(
-            find.byType(TextFormField).first, '0905123456789');
-        await tester.pumpAndSettle();
-
-        // Error text: Replaced by Phone number must have 10 digits.
-        expect(find.text('Phone number is blank.'), findsNothing);
-        expect(find.text('Phone number must have 10 digits.'), findsOneWidget);
-
-        // Enter a not-number text to phone number input
-        await tester.enterText(find.byType(TextFormField).first, 'test_phone');
-        await tester.pumpAndSettle();
-
-        // Error text: Replaced by Phone number must be a string of digits.
-        expect(find.text('Phone number must have 10 digits.'), findsNothing);
-        expect(find.text('Phone number must be a string of digits.'),
-            findsOneWidget);
-
-        // Enter a valid phone number input
-        await tester.enterText(find.byType(TextFormField).first, '0905123456');
-        await tester.pumpAndSettle();
-
-        // Error text: Null
-        expect(find.text('Phone number must be a string of digits.'),
-            findsNothing);
-        expect(find.text('Phone number must have 10 digits.'), findsNothing);
-        expect(find.text('Phone number is blank.'), findsNothing);
-      });
-    });
-
-    group('password input -', () {
-      testWidgets(
-          'Enter an empty text to password input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a less-than-6-character text to password input
-        await tester.enterText(find.byType(TextFormField).last, '123');
-
-        // Enter an empty text to password input
-        await tester.enterText(find.byType(TextFormField).last, '');
-        await tester.pumpAndSettle();
-
-        // Error text: Password is blank.
-        expect(find.text('Password is blank.'), findsOneWidget);
-      });
-
-      testWidgets(
-          'Enter a less-6-character text to password input then show an error text',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a less-than-6-character text to password input
-        await tester.enterText(find.byType(TextFormField).last, '123');
-        await tester.pumpAndSettle();
-
-        // Error text: Password must be at least 6 characters.
-        expect(find.text('Password must be at least 6 characters.'),
-            findsOneWidget);
-      });
-
-      testWidgets('Enter valid text to password input after an invalid one',
-          (tester) async {
-        // Run app with LoginScreen()
-        await tester.pumpWidget(loginScreen);
-
-        // Enter a less-than-6-character text to password input
-        await tester.enterText(find.byType(TextFormField).last, '123');
-        await tester.pumpAndSettle();
-
-        // Error text: Password must be at least 6 characters.
-        expect(find.text('Password must be at least 6 characters.'),
-            findsOneWidget);
-
-        // Enter an empty text to password input
-        await tester.enterText(find.byType(TextFormField).last, '');
-        await tester.pumpAndSettle();
-
-        // Error text: Replaced by Password is blank.
-        expect(
-            find.text('Password must be at least 6 characters.'), findsNothing);
-        expect(find.text('Password is blank.'), findsOneWidget);
-
-        // Enter a valid password input
-        await tester.enterText(find.byType(TextFormField).last, '123456789');
-        await tester.pumpAndSettle();
-
-        // Error text: Null
-        expect(find.text('Password is blank.'), findsNothing);
-        expect(
-            find.text('Password must be at least 6 characters.'), findsNothing);
-      });
-    });
+        // call onEditCompleted function
+        expect(log.length, 1);
+      },
+    );
   });
 }
