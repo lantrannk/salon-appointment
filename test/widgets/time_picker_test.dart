28487a998c837fa4a8df8320d50cb6e9bdbd72f7
Lan Tran <lan.tran@asnet.com.vn>
2023-08-10 11:34:10 +0700

    Fix: test common widget - date picker, time picker

--- test/widgets/time_picker_test.dart
+++ test/widgets/time_picker_test.dart
@@ -1,49 +1,40 @@
 import 'dart:io' as io;
 
-import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
 import 'package:salon_appointment/core/generated/l10n.dart';
-import 'package:salon_appointment/features/appointments/bloc/appointment_bloc.dart';
-import 'package:salon_appointment/features/appointments/model/appointment.dart';
-import 'package:salon_appointment/features/appointments/screens/new_appointment_screen.dart';
+import 'package:salon_appointment/core/widgets/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import '../mock_data/mock_data.dart';
-
-class MockAppointmentBloc extends Mock implements AppointmentBloc {}
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
   io.HttpOverrides.global = null;
 
-  late AppointmentBloc appointmentBloc;
-  late Widget newAppointmentScreen;
-  late List<AppointmentState> expectedStates;
+  late Widget timePickerWidget;
   late Finder fromTextFinder;
   late Finder toTextFinder;
   late Finder startTimeFinder;
   late Finder endTimeFinder;
-
-  final appointment = Appointment.fromJson(const {
-    'date': '2023-08-15T10:55:00.000',
-    'startTime': '2023-08-15T20:00:00.000',
-    'endTime': '2023-08-15T20:30:00.000',
-    'userId': '1',
-    'services': 'Non-Invasive Body Contouring',
-    'description': 'Nothing to write.',
-    'isCompleted': false,
-    'id': '80'
-  });
+  late List<int> startTimeLog;
+  late List<int> endTimeLog;
+  late VoidCallback onStartTimePressed;
+  late VoidCallback onEndTimePressed;
 
   setUpAll(() async {
     SharedPreferences.setMockInitialValues({});
 
-    appointmentBloc = MockAppointmentBloc();
-    newAppointmentScreen = MediaQuery(
+    fromTextFinder = find.text('From:');
+    toTextFinder = find.text('To:');
+    startTimeFinder = find.widgetWithText(OutlinedButton, '10:00');
+    endTimeFinder = find.widgetWithText(OutlinedButton, '10:30');
+
+    startTimeLog = [];
+    endTimeLog = [];
+    onStartTimePressed = () => startTimeLog.add(0);
+    onEndTimePressed = () => endTimeLog.add(0);
+
+    timePickerWidget = MediaQuery(
       data: const MediaQueryData(),
       child: MaterialApp(
         localizationsDelegates: const [
@@ -53,88 +44,71 @@ void main() {
           GlobalCupertinoLocalizations.delegate,
         ],
         supportedLocales: S.delegate.supportedLocales,
-        home: BlocProvider.value(
-          value: appointmentBloc,
-          child: NewAppointmentScreen(
-            selectedDay: DateTime(2023, 08, 15),
-            appointment: appointment,
+        home: Scaffold(
+          body: SizedBox(
+            child: TimePicker(
+              startTime: DateTime(2023, 08, 25, 10, 0),
+              endTime: DateTime(2023, 08, 25, 10, 30),
+              onStartTimePressed: onStartTimePressed,
+              onEndTimePressed: onEndTimePressed,
+            ),
           ),
         ),
       ),
     );
-
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setString('user', MockDataUser.adminUserJson);
-
-    expectedStates = [
-      UserLoaded(MockDataUser.adminUser),
-    ];
-    whenListen(
-      appointmentBloc,
-      Stream.fromIterable(expectedStates),
-      initialState: UserLoaded(MockDataUser.adminUser),
-    );
-
-    fromTextFinder = find.text('From:');
-    toTextFinder = find.text('To:');
-    startTimeFinder = find.widgetWithText(OutlinedButton, '20:00');
-    endTimeFinder = find.widgetWithText(OutlinedButton, '20:30');
   });
 
-  testWidgets(
-    'New Appointment Screen has one time picker widget',
-    (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpWidget(newAppointmentScreen);
-        await tester.pumpAndSettle();
+  group('test time picker widget has', () {
+    testWidgets('a from text', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
 
-        // Date picker widget has 4 elements:
-        // from text, start time outlined button, to text, end time outlined button
-        expect(fromTextFinder, findsOneWidget);
-        expect(toTextFinder, findsOneWidget);
-        expect(startTimeFinder, findsOneWidget);
-        expect(endTimeFinder, findsOneWidget);
-      });
-    },
-  );
+      expect(fromTextFinder, findsOneWidget);
+    });
 
-  testWidgets(
-    'Show time picker dialog when pressing start time outlined button',
-    (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpWidget(newAppointmentScreen);
-        await tester.pumpAndSettle();
+    testWidgets('a to text', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
 
-        // Press start time outlined button
-        await tester.tap(startTimeFinder);
-        await tester.pumpAndSettle();
+      expect(toTextFinder, findsOneWidget);
+    });
 
-        // Show time picker dialog when pressing start time outlined button
-        expect(find.byType(TimePickerDialog), findsOneWidget);
-      });
-    },
-  );
+    testWidgets('a start time outlined button', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
 
-  testWidgets(
-    'Show another start time outlined button after selecting on time picker dialog',
-    (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpWidget(newAppointmentScreen);
-        await tester.pumpAndSettle();
+      expect(startTimeFinder, findsOneWidget);
+    });
 
-        // Press start time outlined button
-        await tester.tap(startTimeFinder);
-        await tester.pumpAndSettle();
+    testWidgets('a end time outlined button', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
 
-        // Press time and OK
-        await tester.tap(find.text('AM'));
-        await tester.tap(find.text('OK'));
-        await Future.delayed(const Duration(seconds: 3));
-        await tester.pumpAndSettle();
+      expect(endTimeFinder, findsOneWidget);
+    });
+  });
 
-        // Show another start time outlined button after selecting
-        expect(find.widgetWithText(OutlinedButton, '08:00'), findsOneWidget);
-      });
-    },
-  );
+  group('test start time pressed then', () {
+    testWidgets('call onStartTimePressed function 1 time', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
+
+      await tester.tap(startTimeFinder);
+      await tester.pumpAndSettle();
+
+      expect(startTimeLog.length, 1);
+    });
+  });
+
+  group('test end time pressed then', () {
+    testWidgets('call onTimePressed function 1 time', (tester) async {
+      await tester.pumpWidget(timePickerWidget);
+      await tester.pump();
+
+      await tester.tap(endTimeFinder);
+      await tester.pumpAndSettle();
+
+      expect(endTimeLog.length, 1);
+    });
+  });
 }
