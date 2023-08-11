8ab1b06112e85620fefbe7a189729a6dcd4be193
Lan Tran <lan.tran@asnet.com.vn>
2023-08-11 14:00:55 +0700

    create extension to pump widget when testing

---
+++ test/helpers/pump_app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:salon_appointment/core/generated/l10n.dart';
+
+extension PumpApp on WidgetTester {
+  Future<void> pumpApp(Widget widget) {
+    return pumpWidget(
+      MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        home: widget,
+      ),
+    );
+  }
+}
