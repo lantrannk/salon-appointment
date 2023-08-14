395e101302742ffbd75795bee5f0e67d4351da65
Lan Tran <lan.tran@asnet.com.vn>
2023-08-14 18:38:29 +0700

    test user api error cases

--- test/apis/user_api_test.dart
+++ test/apis/user_api_test.dart
@@ -30,5 +30,105 @@ void main() {
         expect(await userApi.getUsers(client), MockDataUser.allUsersJson);
       },
     );
+
+    test(
+      'get users error 304',
+      timeout: const Timeout(Duration(seconds: 5)),
+      () async {
+        final client = MockClient();
+        final userApi = UserApi();
+
+        when(
+          () => client.get(
+            Uri.parse('https://63ab8e97fdc006ba60609b9b.mockapi.io/users'),
+          ),
+        ).thenAnswer(
+          (_) async => http.Response(
+            'Not Modified',
+            304,
+          ),
+        );
+
+        expect(
+          await userApi.getUsers(client),
+          'Not Modified',
+        );
+      },
+    );
+
+    test(
+      'get users error 400',
+      timeout: const Timeout(Duration(seconds: 5)),
+      () async {
+        final client = MockClient();
+        final userApi = UserApi();
+
+        when(
+          () => client.get(
+            Uri.parse('https://63ab8e97fdc006ba60609b9b.mockapi.io/users'),
+          ),
+        ).thenAnswer(
+          (_) async => http.Response(
+            'Bad Request',
+            400,
+          ),
+        );
+
+        expect(
+          await userApi.getUsers(client),
+          'Bad Request',
+        );
+      },
+    );
+
+    test(
+      'get users error 404',
+      timeout: const Timeout(Duration(seconds: 5)),
+      () async {
+        final client = MockClient();
+        final userApi = UserApi();
+
+        when(
+          () => client.get(
+            Uri.parse('https://63ab8e97fdc006ba60609b9b.mockapi.io/users'),
+          ),
+        ).thenAnswer(
+          (_) async => http.Response(
+            'Not Found',
+            404,
+          ),
+        );
+
+        expect(
+          await userApi.getUsers(client),
+          'Not Found',
+        );
+      },
+    );
+
+    test(
+      'get users error 504',
+      timeout: const Timeout(Duration(seconds: 5)),
+      () async {
+        final client = MockClient();
+        final userApi = UserApi();
+
+        when(
+          () => client.get(
+            Uri.parse('https://63ab8e97fdc006ba60609b9b.mockapi.io/users'),
+          ),
+        ).thenAnswer(
+          (_) async => http.Response(
+            'Gateway Timeout',
+            504,
+          ),
+        );
+
+        expect(
+          await userApi.getUsers(client),
+          'Gateway Timeout',
+        );
+      },
+    );
   });
 }
