9c96b42922a53744cc4c3d8cf850202ac9fac6a4
Lan Tran <lan.tran@asnet.com.vn>
2023-08-15 18:52:39 +0700

    define constants for api error message

---
+++ test/constants/api_error_message.dart
@@ -0,0 +1,9 @@
+class ApiErrorMessage {
+  static const String notModified = 'Not Modified';
+
+  static const String badRequest = 'Bad Request';
+
+  static const String notFound = 'Not Found';
+
+  static const String gatewayTimeout = 'Gateway Timeout';
+}
