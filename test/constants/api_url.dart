cef04ac3ef04eb6a04dce494bc17bd06604b1dc3
Lan Tran <lan.tran@asnet.com.vn>
2023-08-18 16:00:40 +0700

    define constant url

---
+++ test/constants/api_url.dart
@@ -0,0 +1,7 @@
+import 'package:salon_appointment/core/constants/constants.dart';
+
+final allUsersUrl = Uri.parse('$apiUrl/users');
+
+final allAppointmentsUrl = Uri.parse('$apiUrl/appointments');
+
+final appointmentUrl = Uri.parse('$apiUrl/appointments/84');
