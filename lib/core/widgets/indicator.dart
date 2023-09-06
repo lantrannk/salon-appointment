dbb7373d68c8763d3ec3f172710deb732e90539e
Lan Tran <lan.tran@asnet.com.vn>
2023-09-06 19:15:41 +0700

    update indicator widget

--- lib/core/widgets/indicator.dart
+++ lib/core/widgets/indicator.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 class SAIndicator extends StatelessWidget {
   const SAIndicator({
     this.height,
+    this.color,
     super.key,
   });
 
   final double? height;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +18,7 @@ class SAIndicator extends StatelessWidget {
       height: height,
       child: Center(
         child: CircularProgressIndicator(
-          color: colorScheme.onSurface,
+          color: color ?? colorScheme.onPrimary,
         ),
       ),
     );
@@ -35,9 +37,20 @@ class LoadingIndicator {
     isLoading = true;
     showDialog(
       context: context,
-      barrierColor: Theme.of(context).colorScheme.onBackground,
-      builder: (context) => SAIndicator(
-        height: height,
+      builder: (context) => Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Theme.of(context).colorScheme.primary.withOpacity(0.05),
+              Theme.of(context).colorScheme.secondary.withOpacity(0.05),
+            ],
+          ),
+        ),
+        child: SAIndicator(
+          height: height,
+        ),
       ),
     );
   }
