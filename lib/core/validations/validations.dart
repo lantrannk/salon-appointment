e0694a4e2fd8a099937b4ba098e567c1c9ae0641
Lan Tran <lan.tran@asnet.com.vn>
2023-09-11 08:33:30 +0700

    update phone number validation

--- lib/core/validations/validations.dart
+++ lib/core/validations/validations.dart
@@ -1,29 +1,34 @@
 import '../../features/auth/model/user.dart';
+import '../constants/constants.dart';
 
 class FormValidation {
   static String? isValidPhoneNumber(String? phoneNumber) {
-    if (phoneNumber == '') {
+    final RegExp phoneNumberRegExp = RegExp(phoneNumberRegExpPattern);
+
+    if (phoneNumber!.isEmpty) {
       return 'Phone number is blank.';
-    } else if (phoneNumber!.length != 10) {
-      return 'Phone number must have 10 digits.';
-    } else if (int.tryParse(phoneNumber) == null) {
-      return 'Phone number must be a string of digits.';
+    } else if (!phoneNumberRegExp.hasMatch(phoneNumber)) {
+      return 'Invalid phone number.';
     }
     return null;
   }
 
   static String? isValidPassword(String? password) {
-    if (password == '') {
+    if (password!.isEmpty) {
       return 'Password is blank.';
-    } else if (password!.length < 6) {
-      return 'Password must be at least 6 characters.';
+    } else if (password.length < 6) {
+      return 'Invalid password.';
     }
     return null;
   }
 
   static bool isLoginSuccess(
-      List<User> users, String? phoneNumber, String? password) {
-    return users
-        .any((e) => e.phoneNumber == phoneNumber && e.password == password);
+    List<User> users,
+    String? phoneNumber,
+    String? password,
+  ) {
+    return users.any(
+      (e) => e.phoneNumber == phoneNumber && e.password == password,
+    );
   }
 }
