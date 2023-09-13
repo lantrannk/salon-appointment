761fcee073485ccd2c87dd2a7a95d043de8d9653
lantrannk <lantrannk@gmail.com>
2023-09-14 06:33:33 +0700

    test password validation

--- test/screens/login_screen_test.dart
+++ test/screens/login_screen_test.dart
@@ -74,30 +74,6 @@ void main() {
   );
 
   testWidgets(
-    'Enter a valid text into phone number text form field',
-    (tester) async {
-      await tester.runAsync(() async {
-        await tester.pumpApp(loginScreen);
-        await tester.pumpAndSettle();
-
-        // Show text form field with hint text 'Phone number'
-        expect(phoneNumberInputFinder, findsOneWidget);
-
-        // Enter a valid phone number
-        // Valid phone number includes:
-        // Phone numbers must contain 10 digits
-        // In case country code us used, it can be 12 digits
-        // No space or no characters allowed between digits
-        await tester.enterText(phoneNumberInputFinder, '0794542105');
-        await tester.pumpAndSettle();
-
-        // Show valid text
-        expect(find.text('0794542105'), findsOneWidget);
-      });
-    },
-  );
-
-  testWidgets(
     'Show error text when enter an invalid text into phone number text form field',
     (tester) async {
       await tester.runAsync(() async {
@@ -107,6 +83,11 @@ void main() {
         // Show text form field with hint text 'Phone number'
         expect(phoneNumberInputFinder, findsOneWidget);
 
+        // Enter an invalid phone number
+        // Valid phone number includes:
+        // Phone numbers must contain 10 digits
+        // In case country code us used, it can be 12 digits
+        // No space or no characters allowed between digits
         await tester.enterText(phoneNumberInputFinder, '090-234-4567');
         await tester.pumpAndSettle();
 
@@ -117,7 +98,27 @@ void main() {
   );
 
   testWidgets(
-    'Show password text form field',
+    'Enter a valid text into phone number text form field',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpApp(loginScreen);
+        await tester.pumpAndSettle();
+
+        // Show text form field with hint text 'Phone number'
+        expect(phoneNumberInputFinder, findsOneWidget);
+
+        // Enter a valid phone number
+        await tester.enterText(phoneNumberInputFinder, '0794542105');
+        await tester.pumpAndSettle();
+
+        // Show valid text
+        expect(find.text('0794542105'), findsOneWidget);
+      });
+    },
+  );
+
+  testWidgets(
+    'Show error text when enter an invalid text into password text form field',
     (tester) async {
       await tester.runAsync(() async {
         await tester.pumpApp(loginScreen);
@@ -125,6 +126,40 @@ void main() {
 
         // Show text form field with hint text 'Password'
         expect(passwordInputFinder, findsOneWidget);
+
+        // Enter an invalid password
+        // Valid phone number includes:
+        // At least 8 characters
+        // At least 1 Uppercase
+        // At least 1 lowercase
+        // At least 1 numberic character
+        // At least 1 special character
+        // Common Allow Character ( ! @ # $ & * ~ )
+        await tester.enterText(passwordInputFinder, '12345678');
+        await tester.pumpAndSettle();
+
+        // Show error text
+        expect(find.text('Invalid password.'), findsOneWidget);
+      });
+    },
+  );
+
+  testWidgets(
+    'Enter a valid text into password text form field',
+    (tester) async {
+      await tester.runAsync(() async {
+        await tester.pumpApp(loginScreen);
+        await tester.pumpAndSettle();
+
+        // Show text form field with hint text 'Password'
+        expect(passwordInputFinder, findsOneWidget);
+
+        // Enter a valid password
+        await tester.enterText(passwordInputFinder, 'qwQW12!@');
+        await tester.pumpAndSettle();
+
+        // Error text not found
+        expect(find.text('Invalid password.'), findsNothing);
       });
     },
   );
