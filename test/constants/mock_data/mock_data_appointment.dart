ced493595102a79e09fa2cb90b91d2ab037ec7a5
lantrannk <lantrannk@gmail.com>
2023-09-14 06:47:57 +0700

    add more appointment contants to test

--- test/constants/mock_data/mock_data_appointment.dart
+++ test/constants/mock_data/mock_data_appointment.dart
@@ -379,4 +379,97 @@ class MockDataAppointment {
       'id': '80'
     },
   ].map((e) => Appointment.fromJson(e)).toList();
+
+  static final List<Appointment> fullAppointments = [
+    {
+      'date': '2023-07-20T17:01:05.738070',
+      'startTime': '2023-07-20T18:00:00.000',
+      'endTime': '2023-07-20T18:30:00.000',
+      'userId': '2',
+      'services': 'Neck & Shoulders',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '1'
+    },
+    {
+      'date': '2023-07-20T00:00:00.000',
+      'startTime': '2023-07-20T18:15:00.000',
+      'endTime': '2023-07-20T18:45:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '2'
+    },
+    {
+      'date': '2023-07-20T00:00:00.000',
+      'startTime': '2023-07-20T18:05:00.000',
+      'endTime': '2023-07-20T18:35:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '3'
+    },
+    {
+      'date': '2023-07-20T00:00:00.000',
+      'startTime': '2023-07-20T18:00:00.000',
+      'endTime': '2023-07-20T18:30:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '4'
+    },
+    {
+      'date': '2023-07-20T00:00:00.000',
+      'startTime': '2023-07-20T18:20:00.000',
+      'endTime': '2023-07-20T18:50:00.000',
+      'userId': '1',
+      'services': 'Back',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '5'
+    },
+    {
+      'date': '2023-07-19T00:00:00.000',
+      'startTime': '2023-07-19T18:00:00.000',
+      'endTime': '2023-07-19T18:30:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '6'
+    },
+    {
+      'date': '2023-07-19T00:00:00.000',
+      'startTime': '2023-07-19T18:00:00.000',
+      'endTime': '2023-07-19T18:30:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '7'
+    },
+    {
+      'date': '2023-07-19T00:00:00.000',
+      'startTime': '2023-07-19T18:00:00.000',
+      'endTime': '2023-07-19T18:30:00.000',
+      'userId': '1',
+      'services': 'Non-Invasive Body Contouring',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '8'
+    },
+    {
+      'date': '2023-07-19T00:00:00.000',
+      'startTime': '2023-07-19T18:00:00.000',
+      'endTime': '2023-07-19T18:30:00.000',
+      'userId': '1',
+      'services': 'Back',
+      'description': 'Nothing to write.',
+      'isCompleted': false,
+      'id': '9'
+    }
+  ].map((e) => Appointment.fromJson(e)).toList();
 }
