63538b06b78a1ffc5c5f4c915e84e7b826a06beb
Lan Tran <lan.tran@asnet.com.vn>
2023-09-27 18:07:13 +0700

    create bloc for calendar screen

---
+++ lib/features/appointments/screens/calendar/bloc/calendar_state.dart
@@ -0,0 +1,42 @@
+part of 'calendar_bloc.dart';
+
+class CalendarState extends Equatable {
+  const CalendarState({
+    required this.focusedDay,
+    required this.selectedDay,
+    this.appointments = const [],
+    this.status = LoadStatus.loadInProgress,
+    this.error,
+  });
+
+  final List<Appointment> appointments;
+  final DateTime focusedDay;
+  final DateTime selectedDay;
+  final LoadStatus status;
+  final String? error;
+
+  @override
+  List<Object?> get props => [
+        appointments,
+        focusedDay,
+        selectedDay,
+        status,
+        error,
+      ];
+
+  CalendarState copyWith({
+    List<Appointment>? appointments,
+    DateTime? focusedDay,
+    DateTime? selectedDay,
+    LoadStatus? status,
+    String? error,
+  }) {
+    return CalendarState(
+      appointments: appointments ?? this.appointments,
+      focusedDay: focusedDay ?? this.focusedDay,
+      selectedDay: selectedDay ?? this.selectedDay,
+      status: status ?? this.status,
+      error: error ?? this.error,
+    );
+  }
+}
