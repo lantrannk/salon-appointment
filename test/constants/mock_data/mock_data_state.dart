cb80873c849187e56a83a2f269584f8df64c5e51
Lan Tran <lan.tran@asnet.com.vn>
2023-10-11 18:21:47 +0700

    define initial appointment form state

--- test/constants/mock_data/mock_data_state.dart
+++ test/constants/mock_data/mock_data_state.dart
@@ -1,4 +1,5 @@
 import 'package:salon_appointment/features/appointments/screens/appointment/bloc/appointment_bloc.dart';
+import 'package:salon_appointment/features/appointments/screens/appointment_form/bloc/appointment_form_bloc.dart';
 import 'package:salon_appointment/features/appointments/screens/calendar/bloc/calendar_bloc.dart';
 
 class MockDataState {
@@ -17,5 +18,12 @@ class MockDataState {
     selectedDay: DateTime.now(),
     error: null,
     status: LoadStatus.loadInProgress,
+  );
+
+  static final initialAppointmentFormState = AppointmentFormState(
+    date: DateTime.now(),
+    startTime: DateTime.now(),
+    endTime: DateTime.now(),
+    status: AppointmentFormStatus.initInProgress,
   );
 }
