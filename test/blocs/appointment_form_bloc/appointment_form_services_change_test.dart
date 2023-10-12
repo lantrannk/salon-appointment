5703aa30e10b8e8e0cf1f026760594d28ce662ec
Lan Tran <lan.tran@asnet.com.vn>
2023-10-12 17:53:18 +0700

    remove unnecessary import directive

--- test/blocs/appointment_form_bloc/appointment_form_services_change_test.dart
+++ test/blocs/appointment_form_bloc/appointment_form_services_change_test.dart
@@ -3,7 +3,6 @@ import 'dart:io' as io;
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
-import 'package:salon_appointment/features/appointments/model/appointment.dart';
 import 'package:salon_appointment/features/appointments/repository/appointment_repository.dart';
 import 'package:salon_appointment/features/appointments/screens/appointment_form/bloc/appointment_form_bloc.dart';
 import 'package:salon_appointment/features/auth/repository/user_repository.dart';
