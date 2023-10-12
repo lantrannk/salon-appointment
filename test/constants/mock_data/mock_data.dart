26eac446ff42c18c7152a2cdb2fd66eac62c2d11
Lan Tran <lan.tran@asnet.com.vn>
2023-10-12 15:11:46 +0700

    create mock data to test appointment form bloc

--- test/constants/mock_data/mock_data.dart
+++ test/constants/mock_data/mock_data.dart
@@ -1,3 +1,4 @@
 export 'mock_data_appointment.dart';
 export 'mock_data_state.dart';
 export 'mock_data_user.dart';
+export 'mock_date_date_time.dart';
--- test/constants/mock_data/mock_data_state.dart
+++ test/constants/mock_data/mock_data_state.dart
@@ -1,6 +1,9 @@
+import 'package:salon_appointment/core/utils/common.dart';
 import 'package:salon_appointment/features/appointments/screens/appointment/bloc/appointment_bloc.dart';
 import 'package:salon_appointment/features/appointments/screens/appointment_form/bloc/appointment_form_bloc.dart';
 import 'package:salon_appointment/features/appointments/screens/calendar/bloc/calendar_bloc.dart';
+
+import '../constants.dart';
 
 class MockDataState {
   static final initialAppointmentState = AppointmentState(
@@ -25,5 +28,15 @@ class MockDataState {
     startTime: DateTime.now(),
     endTime: DateTime.now(),
     status: AppointmentFormStatus.initInProgress,
+  );
+
+  static final initSuccessAppointmentFormState = AppointmentFormState(
+    user: MockDataUser.adminUser,
+    date: MockDataDateTime.initSuccessDateTime,
+    startTime: MockDataDateTime.initSuccessDateTime,
+    endTime: autoAddHalfHour(MockDataDateTime.initSuccessDateTime),
+    services: MockDataAppointment.appointment.services,
+    description: MockDataAppointment.appointment.description,
+    status: AppointmentFormStatus.initSuccess,
   );
 }
