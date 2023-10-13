afb9535b04d984c19d2f03387ab4fa068f255cc1
Lan Tran <lan.tran@asnet.com.vn>
2023-10-13 17:13:30 +0700

    Update test calendar bloc

--- test/constants/mock_data/mock_data_state.dart
+++ test/constants/mock_data/mock_data_state.dart
@@ -1,4 +1,5 @@
 import 'package:salon_appointment/features/appointments/screens/appointment/bloc/appointment_bloc.dart';
+import 'package:salon_appointment/features/appointments/screens/calendar/bloc/calendar_bloc.dart';
 
 class MockDataState {
   static final initialAppointmentState = AppointmentState(
@@ -8,5 +9,13 @@ class MockDataState {
     selectedDay: DateTime.now(),
     error: null,
     status: Status.loadInProgress,
+  );
+
+  static final initialCalendarState = CalendarState(
+    appointments: const [],
+    focusedDay: DateTime.now(),
+    selectedDay: DateTime.now(),
+    error: null,
+    status: LoadStatus.loadInProgress,
   );
 }
